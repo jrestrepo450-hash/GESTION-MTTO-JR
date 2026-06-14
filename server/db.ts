dcaa7fc9e9fd702079136905c462f313c8d3c977
julian daniel restrepo foronda <j.restrepo450@pascualbravo.edu.co>
2026-06-14 13:46:09 -0500

    fix: 5

--- server/db.ts
+++ server/db.ts
@@ -3,7 +3,7 @@ import pg from "pg";
 import * as schema from "@shared/schema";
 
 const { Pool } = pg;
-
+process.env.DATABASE_URL = "file:./local.db";
 if (!process.env.DATABASE_URL) {
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
