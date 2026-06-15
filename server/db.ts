747762438d3fb10eff6638866404eea7b27a58a1
julian daniel restrepo foronda <j.restrepo450@pascualbravo.edu.co>
2026-06-15 17:17:12 -0500

    fix: 58564

--- server/db.ts
+++ server/db.ts
@@ -2,6 +2,6 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from "@shared/schema";
 
-// Esto crea automáticamente un archivo físico llamado 'local.db' en tu proyecto
+// Forzamos a que siempre use el archivo local.db de SQLite
 const sqlite = new Database('local.db');
 export const db = drizzle(sqlite, { schema });
